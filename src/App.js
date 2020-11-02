5ed251302bd51359e53b19215e500f69907f7956
kaurk <thind1kamal@gmail.com>
2020-11-02 10:56:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
-import Weather from "Weather";
+import Weather from "./Weather";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-        <Weather/>
-      </header>
-    </div>
+    
+      <Weather />
+    
   );
 }
 
