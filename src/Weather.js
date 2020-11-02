23da95407154f04a1d6a7aace6fc7d1df1030487
kaurk <thind1kamal@gmail.com>
2020-11-02 16:21:57 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,10 +2,30 @@ import React from "react";
 //import axios from "axios";
 
 export default function Weather(){
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return(
        <div>
-         alert("Hello World")
-                   </div>
+        <form >
+            <input 
+            className="city-input"
+            type="text"
+            placeholder="type a city name...."
+            />
+            <input type="submit"
+            value="search" />
+            </form>
+            </div>
 
     );
 }
