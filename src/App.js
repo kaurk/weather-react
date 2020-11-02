4a855e51d3dc6aa6e9e6e9a19a057b18879fee41
kaurk <thind1kamal@gmail.com>
2020-11-02 13:14:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 import React from "react";
 import './App.css';
 import Weather from "./Weather";
+import Footer from "./Footer";
+
 function App() {
   return (
-    
+    <div className="App">
       <Weather />
+      < Footer />
+      </div>
     
   );
 }
