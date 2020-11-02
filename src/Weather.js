4b7ab097c02a2ac217586e4ab5b4f4f1f4fb1774
kaurk <thind1kamal@gmail.com>
2020-11-02 16:55:02 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,9 @@
 import React from "react";
-//import axios from "axios";
+
+import Forecast from "./Forecast";
 
 export default function Weather(props){
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+         
     return(
        <div>
         <form >
@@ -29,6 +19,17 @@ export default function Weather(props){
                 <li className="city-heading">{props.city}</li>
          <li>    Monday,16:31  </li>
             </ul>
+
+            <h3>3 HOURS FORECAST:</h3>
+        <div className="forecast-frame">
+          <div className="row">
+            <Forecast />
+            <Forecast />
+            <Forecast />
+            <Forecast />
+            <Forecast />
+          </div>
+          </div>
             </div>
 
     );
