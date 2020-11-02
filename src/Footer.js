4ea3444c2e6324143ac1c88557f4495630dfe3b6
kaurk <thind1kamal@gmail.com>
2020-11-02 13:14:05 +0100

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+
+export default function Footer(){
+    return(
+<div className="Footer">
+    <a href="https://github.com/kaurk/weather-react"
+    rel="noopener noreferrer"
+    target="_blank">
+        Open-source code " "
+    </a>
+    <span> by Kamaljit Kaur </span>
+</div>
+    );
+}
